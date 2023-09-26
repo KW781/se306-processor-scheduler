1a9bcc9ce8bdde4033916ac5dfbe15011c9b5a1c
KW781 <aarnobguha@gmail.com>
2023-09-26 15:13:21 +1300

    feat: :sparkles: added model code from previous project

--- src/main/java/com/example/project2project2team16/HelloApplication.java
+++ src/main/java/com/example/project2project2team16/HelloApplication.java
@@ -1,9 +1,11 @@
 package com.example.project2project2team16;
 
+import com.example.project2project2team16.model.DotFileParser;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.graphstream.graph.Graph;
 
 import java.io.IOException;
 
@@ -18,6 +20,8 @@ public class HelloApplication extends Application {
     }
 
     public static void main(String[] args) {
+        System.out.println(args[0]); // placeholder to make sure cmd line argument is read
+        Graph taskGraph = DotFileParser.parseDotFile(args[0]);
         launch();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.project2project2team16 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+    requires gs.core;
+
+
     opens com.example.project2project2team16 to javafx.fxml;
     exports com.example.project2project2team16;
 }
