4ca4ee471b47f191d1f6b13b26e3fc52588a0cad
KW781 <aarnobguha@gmail.com>
2023-09-26 14:39:31 +1300

    feat: :sparkles: replaced original project with javafx project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.example.project2project2team16 {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+                            
+    opens com.example.project2project2team16 to javafx.fxml;
+    exports com.example.project2project2team16;
+}
