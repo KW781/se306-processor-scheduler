d7a971d23e9481e0197a31ab8d2c75828bb43e15
Hugo20038 <hmil830@aucklanduni.ac.nz>
2023-09-27 17:34:27 +1300

    Feat - Implemented use of GraphStream graph for the task graph.

--- src/main/java/com/example/project2project2team16/searchers/ScheduleNode.java
+++ src/main/java/com/example/project2project2team16/searchers/ScheduleNode.java
@@ -1,6 +1,9 @@
 package com.example.project2project2team16.searchers;
 
 import javafx.util.Pair;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.Edge;
 
 import java.util.*;
 
@@ -8,19 +11,21 @@ public class ScheduleNode {
 
     //Task Id as key, data is pair of processor run on and end time.
     Map<String, Pair<Integer, Integer>> visited;
-    Set<TaskNode> availableTasks;
+    Set<Node> availableTasks;
     List<Integer> processorEndTimes;
     Integer processorCount;
 
-    public ScheduleNode(Integer processorCount) {
+    public ScheduleNode(Integer processorCount, Set<Node> startingTasks) {
+        this.availableTasks = startingTasks;
         this.processorCount = processorCount;
         processorEndTimes = new ArrayList<>(processorCount);
         visited = new HashMap<>();
     }
 
-    public ScheduleNode(ScheduleNode copy, TaskNode newTask, Integer processor) {
-        this.visited = copy.visited;
-        this.processorEndTimes = copy.processorEndTimes;
+    public ScheduleNode(ScheduleNode copy, Node newTask, Integer processor) {
+        this.visited = new HashMap<>(copy.visited);
+        this.availableTasks = new HashSet<>(copy.availableTasks);
+        this.processorEndTimes = new ArrayList<>(copy.processorEndTimes);
 
         AddTask(newTask, processor);
     }
@@ -28,7 +33,7 @@ public class ScheduleNode {
     public List<ScheduleNode> GenerateNeighbours() {
         List<ScheduleNode> neighbours = new ArrayList<>();
 
-        for (TaskNode task : availableTasks) {
+        for (Node task : availableTasks) {
             for (int i = 0; i < processorCount; i++) {
                 neighbours.add(new ScheduleNode(this, task, i));
             }
@@ -50,11 +55,4 @@ public class ScheduleNode {
 
         return result;
     }
-
-    private void AddTask(TaskNode newTask, Integer processor) {
-        //TODO
-    }
-
-
-
 }
