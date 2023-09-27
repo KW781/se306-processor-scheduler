dab182bc3ae24829810b175eddcda5e56ba16ad2
Hugo20038 <hmil830@aucklanduni.ac.nz>
2023-09-27 16:07:17 +1300

    Feat - Implemented basic DFS searcher to produce an optimal schedule. Currenly coupled with this specific search problem but as we are only solving this problem that is not an issue. Can be changed later with interfaces.

---
+++ src/main/java/com/example/project2project2team16/searchers/DFSSearcher.java
@@ -0,0 +1,59 @@
+package com.example.project2project2team16.searchers;
+
+import java.util.Collection;
+import java.util.Stack;
+
+/*
+ * Basic Searcher producing an optional solution using DFS. Can be extended from for more advanced search algorithms.
+ */
+public class DFSSearcher {
+    SchedulingProblem problem;
+
+    //Needs to be list type in the future.
+    Stack<ScheduleNode> frontier;
+    ScheduleNode optimal = null;
+    Integer currentOptimalTime;
+    public DFSSearcher(SchedulingProblem problem) {
+        this.problem = problem;
+        InitialiseFrontier();
+        AddToFrontier(problem.GetStartNodes());
+    }
+
+    //Exhaustive search
+    public ScheduleNode Search() {
+
+        while (!IsFrontierEmpty()) {
+            ScheduleNode nextNode = frontier.pop();
+
+            if (problem.IsGoal(nextNode)) {
+                UpdateOptimal(nextNode);
+            }
+            else {
+                AddToFrontier(problem.GetNeighbourStates(nextNode));
+            }
+        }
+
+        return optimal;
+    }
+
+    private void InitialiseFrontier() {
+        frontier = new Stack<ScheduleNode>();
+    }
+
+    private boolean IsFrontierEmpty() {
+        return frontier.isEmpty();
+    }
+
+    private void AddToFrontier(Collection<ScheduleNode> newNodes) {
+        frontier.addAll(newNodes);
+    }
+
+    private void UpdateOptimal(ScheduleNode newSolution) {
+        Integer newSolutionTime = newSolution.GetValue();
+
+        if (optimal == null || newSolutionTime < currentOptimalTime) {
+            optimal = newSolution;
+            currentOptimalTime = newSolutionTime;
+        }
+    }
+}
