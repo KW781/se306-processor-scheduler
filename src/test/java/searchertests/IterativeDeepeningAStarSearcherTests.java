f1d18dda6a74c2d8411f731bf41ac199aec9e729
Hugo20038 <hmil830@aucklanduni.ac.nz>
2023-09-28 12:43:11 +1300

    Test - Added a Iterative Deepening A* searcher test class.

---
+++ src/test/java/searchertests/IterativeDeepeningAStarSearcherTests.java
@@ -0,0 +1,16 @@
+package searchertests;
+
+import com.example.project2project2team16.searchers.AStarSearcher;
+import com.example.project2project2team16.searchers.IterativeDeepeningAStarSearcher;
+import com.example.project2project2team16.searchers.SchedulingProblem;
+import org.graphstream.graph.Graph;
+
+public class IterativeDeepeningAStarSearcherTests extends SearcherTests {
+    @Override
+    Integer RunSearch(Graph taskGraph, Integer processorNum) {
+        SchedulingProblem problem = new SchedulingProblem(taskGraph, processorNum);
+        IterativeDeepeningAStarSearcher searcher = new IterativeDeepeningAStarSearcher(problem);
+
+        return searcher.Search().GetValue();
+    }
+}
