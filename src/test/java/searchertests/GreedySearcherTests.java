c34618194a9c68dd211a2f6d3a3dd4b54ae27781
Hugo20038 <hmil830@aucklanduni.ac.nz>
2023-09-28 12:41:15 +1300

    Test - Added a Greedy searcher test class.

---
+++ src/test/java/searchertests/GreedySearcherTests.java
@@ -0,0 +1,16 @@
+package searchertests;
+
+import com.example.project2project2team16.searchers.AStarSearcher;
+import com.example.project2project2team16.searchers.GreedySearcher;
+import com.example.project2project2team16.searchers.SchedulingProblem;
+import org.graphstream.graph.Graph;
+
+public class GreedySearcherTests extends SearcherTests {
+    @Override
+    Integer RunSearch(Graph taskGraph, Integer processorNum) {
+        SchedulingProblem problem = new SchedulingProblem(taskGraph, processorNum);
+        GreedySearcher searcher = new GreedySearcher(problem);
+
+        return searcher.Search().GetValue();
+    }
+}
