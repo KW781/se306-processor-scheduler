9db20e53b6fba42d3ebc9f256cf663b1ec630987
KW781 <aarnobguha@gmail.com>
2023-09-30 22:34:53 +1300

    refactor: :recycle: created app config class to encapsulate cmd line args details

--- src/main/java/com/example/project2project2team16/utils/AppConfig.java
+++ src/main/java/com/example/project2project2team16/utils/AppConfig.java
@@ -1,2 +1,37 @@
-package com.example.project2project2team16.model;public class AppConfig {
+package com.example.project2project2team16.utils;
+
+public class AppConfig {
+    private String inputFilePath;
+    private int numProcessors;
+    private int numCores;
+    private boolean visualize;
+    private String outputFileName;
+
+    public AppConfig(String inputFilePath, int numProcessors, int numCores, boolean visualize, String outputFileName) {
+        this.inputFilePath = inputFilePath;
+        this.numProcessors = numProcessors;
+        this.numCores = numCores;
+        this.visualize = visualize;
+        this.outputFileName = outputFileName;
+    }
+
+    public String getInputFilePath() {
+        return this.inputFilePath;
+    }
+
+    public int getNumProcessors() {
+        return this.numProcessors;
+    }
+
+    public int getNumCores() {
+        return this.numCores;
+    }
+
+    public boolean isVisualized() {
+        return this.visualize;
+    }
+
+    public String getOutputFileName() {
+        return this.outputFileName;
+    }
 }
