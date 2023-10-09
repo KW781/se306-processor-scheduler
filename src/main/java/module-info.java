49f6598df834f8b13b9687edddf713862548ebfa
Andrew Lam <bluey17501@gmail.com>
2023-10-10 00:23:21 +1300

    merged in to main

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,13 @@
 module com.example.project2project2team16 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.swing;
     requires gs.core;
+    requires gs.ui.javafx;
 
 
     opens com.example.project2project2team16 to javafx.fxml;
     exports com.example.project2project2team16;
+    exports com.example.project2project2team16.controllers;
+    opens com.example.project2project2team16.controllers to javafx.fxml;
 }
