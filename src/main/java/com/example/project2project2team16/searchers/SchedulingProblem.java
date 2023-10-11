06b61abff56a1e992dc45dbddcb127e631a21830
Ben Zhou <benzhou2597@gmail.com>
2023-10-12 12:54:50 +1300

    refactor: removed some redundant code

--- src/main/java/com/example/project2project2team16/searchers/SchedulingProblem.java
+++ src/main/java/com/example/project2project2team16/searchers/SchedulingProblem.java
@@ -38,53 +38,30 @@ public class SchedulingProblem {
         return node.GenerateNeighbours();
     }
 
-    private int[] dfs(Node node) {
-        // 0 = cost
-        // 1 = num of tasks
-        int[] result = new int[]{0, 0};
-
-        List<Node> nodeChildren = node.leavingEdges().map(Edge::getTargetNode).collect(Collectors.toList());
-        for (Node child : nodeChildren) {
-            int[] childResult = dfs(child);
-
-            if (childResult[0] > result[0] || (childResult[0] == result[0] && childResult[1] > result[1])) {
-                result = childResult;
-            }
-        }
-
-        result[0] += node.getAttribute("Weight", Double.class).intValue();
-        result[1]++;
-
-        return result;
-    }
-
-    private int GetCriticalPath(Node node) {
+    private int dfs(Node node) {
         int cost = 0;
 
         List<Node> nodeChildren = node.leavingEdges().map(Edge::getTargetNode).collect(Collectors.toList());
-
         for (Node child : nodeChildren) {
-            int[] result = dfs(child);
+            int childCost = dfs(child);
 
-            int freeProcessors = processorCount - result[1];
-
-            int numProcessorsToUse = freeProcessors > 0 ? processorCount - freeProcessors : processorCount;
-
-            if (result[0] / numProcessorsToUse > cost) {
-                cost = result[0];
-            }
+            cost = Math.max(cost, childCost);
         }
+
+        cost += node.getAttribute("Weight", Double.class).intValue();
 
         return cost;
     }
 
-    public Integer initialiseF(ScheduleNode node) {
+    private int GetCriticalPath(Node node) {
+        return dfs(node) - node.getAttribute("Weight", Double.class).intValue();
+    }
+
+    public void initialiseF(ScheduleNode node) {
         for (Node task : node.availableTasks) {
-            int cp = GetCriticalPath(task);
+            int cp = GetCriticalPath(task) + task.getAttribute("Weight", Double.class).intValue();
             node.fValue = Math.max(node.fValue, cp);
         }
-
-        return node.fValue;
     }
 
     public Integer CalculateF(ScheduleNode node) {
