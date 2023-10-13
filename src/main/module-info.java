7f531e3eebc0e613a4e1c2ca7f22e8837bf95a2d
Ben Zhou <benzhou2597@gmail.com>
2023-10-14 11:41:53 +1300

    fix: readded deleted module-info

---
+++ src/main/module-info.java
@@ -0,0 +1,13 @@
+module com.example.project2project2team16 {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires gs.core;
+    requires gs.ui.javafx;
+    requires java.desktop;
+
+
+    opens com.example.project2project2team16 to javafx.fxml;
+    exports com.example.project2project2team16;
+    exports com.example.project2project2team16.controllers;
+    opens com.example.project2project2team16.controllers to javafx.fxml;
+}
