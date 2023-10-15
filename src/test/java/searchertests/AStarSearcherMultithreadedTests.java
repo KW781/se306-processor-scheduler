4c6c92d44bc7521af02e724d12ac49a48281ff80
Hugo20038 <hmil830@aucklanduni.ac.nz>
2023-10-15 22:06:48 +1300

    Test - Added test class for the multithreaded A star searcher.

---
+++ src/test/java/searchertests/AStarSearcherMultithreadedTests.java
@@ -0,0 +1,33 @@
+package searchertests;
+
+import com.example.project2project2team16.VisualisationApplication;
+import com.example.project2project2team16.searchers.AStarSearcherMultithreaded;
+import com.example.project2project2team16.searchers.SchedulingProblem;
+import com.example.project2project2team16.utils.AppConfig;
+import org.graphstream.graph.Graph;
+
+public class AStarSearcherMultithreadedTests extends SearcherTests {
+    @Override
+    public Integer RunSearch(Graph taskGraph, Integer processorNum) {
+        SchedulingProblem problem = new SchedulingProblem(taskGraph, processorNum);
+        AStarSearcherMultithreaded searcher = new AStarSearcherMultithreaded(problem);
+        searcher.InitialiseSearcher();
+
+        AppConfig config = new AppConfig(1);
+        VisualisationApplication.setAppConfig(config);
+
+        return searcher.Search().GetValue();
+    }
+
+    @Override
+    public Integer RunSearch(Graph taskGraph, Integer processorNum, Integer threadCount) {
+        SchedulingProblem problem = new SchedulingProblem(taskGraph, processorNum);
+        AStarSearcherMultithreaded searcher = new AStarSearcherMultithreaded(problem);
+        searcher.InitialiseSearcher();
+
+        AppConfig config = new AppConfig(threadCount);
+        VisualisationApplication.setAppConfig(config);
+
+        return searcher.Search().GetValue();
+    }
+}
