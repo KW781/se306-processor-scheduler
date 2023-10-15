f9791b9694b5d3d4c4968c218faa61726aaa0355
Hugo20038 <hmil830@aucklanduni.ac.nz>
2023-10-15 17:14:45 +1300

    Feat - Created new multithreaded search.

--- src/main/java/com/example/project2project2team16/searchers/AStarSearcher.java
+++ src/main/java/com/example/project2project2team16/searchers/AStarSearcher.java
@@ -10,12 +10,15 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class AStarSearcher extends GreedySearcher {
     Set<ScheduleNode> opened = ConcurrentHashMap.newKeySet();
     Set<ScheduleNode> closed = ConcurrentHashMap.newKeySet();
     ScheduleNode currentOptimal = null;
     Object lock = new Object();
+    List<PriorityBlockingQueue<ScheduleNode>> frontiers = new ArrayList<>();
+    AtomicInteger doneThreads = new AtomicInteger();
 
     public AStarSearcher(SchedulingProblem problem) {
         super(problem);
@@ -26,8 +29,7 @@ public class AStarSearcher extends GreedySearcher {
         frontier = new PriorityBlockingQueue<ScheduleNode>(100000, new ScheduleNodeAStarComparator(problem));
     }
 
-    @Override
-    protected void AddToFrontier(List<ScheduleNode> newNodes) {
+    protected void AddToFrontier(PriorityBlockingQueue<ScheduleNode> frontier, List<ScheduleNode> newNodes) {
         for (int i = newNodes.size() - 1; i >= 0; i--) {
             ScheduleNode newNode = newNodes.get(i);
             if (closed.contains(newNode) || opened.contains(newNode)) {
@@ -39,13 +41,18 @@ public class AStarSearcher extends GreedySearcher {
         }
     }
 
-    @Override
-    protected ScheduleNode GetNextNode() {
-        ScheduleNode node = ((PriorityBlockingQueue<ScheduleNode>) frontier).poll();
+
+    private ScheduleNode GetNextNode(PriorityBlockingQueue<ScheduleNode> frontier) {
+        ScheduleNode node = frontier.poll();
+
+        if (node == null) {
+            return null;
+        }
+
         closed.add(node);
         opened.remove(node);
 
-        if (currentOptimal != null && node.GetValue() > currentOptimal.GetValue()) {
+        if (currentOptimal != null && node.fValue >= currentOptimal.GetValue()) {
             return null;
         }
 
@@ -55,48 +62,64 @@ public class AStarSearcher extends GreedySearcher {
     @Override
     public ScheduleNode Search() {
         Integer threadCount = VisualisationApplication.getThreadCount();
-        CountDownLatch allComplete = new CountDownLatch(threadCount);
+        List<Thread> threads = new ArrayList<>();
+        CountDownLatch completed = new CountDownLatch(threadCount);
 
         for (int i = 0; i < threadCount; i++) {
+            frontiers.add(new PriorityBlockingQueue(10000, new ScheduleNodeAStarComparator(problem)));
+        }
+
+        AddToFrontier(frontiers.get(0), Collections.singletonList(problem.GetStartNode()));
+
+        ScheduleNode intialSearchResult = InitialSearch(threadCount);
+
+        if (intialSearchResult != null) {
+            return intialSearchResult;
+        }
+
+        DistributeFrontiers(threadCount);
+
+        for (int i = 0; i < threadCount; i++) {
+            int threadIndex = i;
+
+            threads.add(
             new Thread(new Runnable() {
                 @Override
                 public void run() {
-                    ThreadSearch(allComplete);
+                    ThreadSearch(threadIndex);
                 }
-            }).start();
+                }));
+
+            threads.get(i).start();
         }
 
-        try {
-            allComplete.await();
+        for (int i = 0; i < threadCount; i++) {
+            try {
+                threads.get(i).join();
+            }
+            catch (InterruptedException e) {
+                System.out.println("Thread waiting failed");
+                System.out.println(e.toString());
+            }
         }
-        catch (InterruptedException e) {
-            System.out.println("Thread waiting failed");
-            System.out.println(e.toString());
-        }
+
+//        try {
+//            completed.await();
+//        }
+//        catch (InterruptedException e) {
+//            System.out.println("Thread waiting failed");
+//            System.out.println(e.toString());
+//        }
 
         return currentOptimal;
     }
 
-    private void ThreadSearch(CountDownLatch complete) {
-        while (!IsFrontierEmpty()) {
-            ScheduleNode nextNode = GetNextNode();
 
-            if (nextNode == null) {
-                break;
-            }
 
-            if (problem.IsGoal(nextNode)) {
-                synchronized (lock) {
-                    if (currentOptimal == null || nextNode.GetValue() < currentOptimal.GetValue()) {
-                        currentOptimal = nextNode;
-                    }
-                }
-            }
-            else {
-                AddToFrontier(problem.GetNeighbourStates(nextNode));
-            }
-        }
 
-        complete.countDown();
-    }
+
+
+
+
+
 }
